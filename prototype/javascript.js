fc612a7d03c08ce79018d76d582aa46e40beb2d8
devchambe <chambelynnemalubay@gmail.com>
2023-12-09 03:02:45 +0800

    WIP: Squircle

--- prototype/javascript.js
+++ prototype/javascript.js
@@ -1,3 +1,10 @@
+function openPopover(){
+    // let snackbar = document.getElementById('snackbar');
+    // snackbar.style.animation = 'slideShow 3s';
+    // snackbar.style.animationDelay = '2s';
+    // snackbar.style.animationFillMode = 'forwards';
+}
+
 function closePopover(){
     let snackbar = document.getElementById('snackbar');
     snackbar.style.animation = 'slideHide 3s';
